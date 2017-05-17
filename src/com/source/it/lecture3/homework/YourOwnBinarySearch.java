65fbf6bf019f300b1b52b5e6d54b047c2acdb9a2
dimon7702506 <7702506@gmail.com>
2017-05-17 16:01:54 +0300

    '20170517_1'

--- src/com/source/it/lecture3/homework/YourOwnBinarySearch.java
+++ src/com/source/it/lecture3/homework/YourOwnBinarySearch.java
@@ -3,7 +3,7 @@ package com.source.it.lecture3.homework;
 import java.util.Arrays;
 
 public class YourOwnBinarySearch {
-    static int[] array = {100, 1, 1, 4, 7, 10, 17, 30, 48};
+    static int[] array = {1, 1, 4, 7, 10, 17, 30, 48};
     static int element = 10;
 
     public static void main(String[] args) {
--- src/com/source/it/lecture4/homework/sort/HugeArraySorter.java
+++ src/com/source/it/lecture4/homework/sort/HugeArraySorter.java
@@ -1,10 +1,16 @@
 package com.source.it.lecture4.homework.sort;
 
+import java.util.Arrays;
+
 public class HugeArraySorter {
     public static void main(String[] args) {
         int[] hugeArray = ArrayCreator.createHugeArray(10);
 
         /*Your implementation here*/
+        System.out.println(Arrays.toString(hugeArray));
+        for (int index = 0; index < hugeArray.length - 1; index++){
+
+        }
 
         for (int index = 0; index < hugeArray.length - 1; index++) {
             if (hugeArray[index] > hugeArray[index + 1]) {
