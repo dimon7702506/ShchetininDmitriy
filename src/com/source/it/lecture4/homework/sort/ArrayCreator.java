ee6b488b2ef841db1a10af7a2967b8445562b24a
unknown <C:\Users\dimon\AppData\Roaming\The Bat!>
2017-05-17 21:42:27 +0300

    '20170517_2'

--- src/com/source/it/lecture4/homework/sort/ArrayCreator.java
+++ src/com/source/it/lecture4/homework/sort/ArrayCreator.java
@@ -5,8 +5,8 @@ import java.util.Random;
 public class ArrayCreator {
     public static int[] createHugeArray(int n) {
         Random random = new Random();
-        //int[] result = new int[random.nextInt(1_000_00) + 1_000_00];
-        int[] result = new int[random.nextInt(10) + 10];
+        int[] result = new int[random.nextInt(1_000_00) + 1_000_00];
+        //int[] result = new int[random.nextInt(10) + 10];
         for(int index = 0; index < result.length; index++) {
             result[index] = random.nextInt(n);
         }
--- src/com/source/it/lecture4/homework/sort/HugeArraySorter.java
+++ src/com/source/it/lecture4/homework/sort/HugeArraySorter.java
@@ -5,12 +5,29 @@ import java.util.Arrays;
 public class HugeArraySorter {
     public static void main(String[] args) {
         int[] hugeArray = ArrayCreator.createHugeArray(10);
+        int[] nullHugeArray = new int[hugeArray.length];
 
-        /*Your implementation here*/
-        System.out.println(Arrays.toString(hugeArray));
-        for (int index = 0; index < hugeArray.length - 1; index++){
+             /*Your implementation here*/
+        /*System.out.println(Arrays.toString(hugeArray));*/
 
+        for (int index = 0; index < hugeArray.length; index++){
+            nullHugeArray[hugeArray[index]] ++;
         }
+
+        /*System.out.println(Arrays.toString(nullHugeArray));*/
+
+        int j = 0;
+
+        for (int index = 0; index < nullHugeArray.length ; index++) {
+            int count = nullHugeArray[index];
+            if (count > 0) {
+                for (int i = 1; i <= count; i++){
+                    hugeArray[j] = index;
+                    j++;
+                }
+            }
+        }
+        /*System.out.println(Arrays.toString(hugeArray));*/
 
         for (int index = 0; index < hugeArray.length - 1; index++) {
             if (hugeArray[index] > hugeArray[index + 1]) {
