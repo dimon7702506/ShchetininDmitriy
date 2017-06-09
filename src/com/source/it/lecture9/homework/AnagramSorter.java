e5b738f6b1e7a5c4d5ab1c53f8c27951614d6d6f
dimon7702506 <7702506@gmail.com>
2017-06-09 14:18:22 +0300

    '20170709'

--- src/com/source/it/lecture9/homework/AnagramSorter.java
+++ src/com/source/it/lecture9/homework/AnagramSorter.java
@@ -1,6 +1,5 @@
 package com.source.it.lecture9.homework;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,24 +7,23 @@ public class AnagramSorter {
     public static void main(String[] args) {
         List<String> strings =
                 Arrays.asList("done", "none", "node", "neno", "onion", "deno", "gram", "ramg", "nioon");
-        List<String> result = Arrays.asList();
 
         for (int i = 0; i < strings.size(); i++){
-            String temp = "";
-            if(i == 0){
-                System.out.println(strings.get(i));
-                temp = strings.get(i);
-                strings.remove(i);
-            }
+            String temp = strings.get(i);
+            strings.set(i, "");
             String etalon = getSort(temp);
-            System.out.println();
-            for (int j = 1; j < strings.size(); j++) {
-                if (etalon == getSort(strings.get(j))) {
-                    System.out.print(strings.get(j) + ", ");
-                    strings.remove(j);
+            System.out.print(temp);
+            for (int j = 0; j < strings.size(); j++) {
+                if (etalon.equals(getSort(strings.get(j)))) {
+                    if (!strings.get(j).isEmpty()) {
+                        System.out.print(", " + strings.get(j));
+                        strings.set(j, "");
+                    }
                 }
             }
-            System.out.println(etalon);
+            if (!temp.isEmpty()) {
+                System.out.println();
+            }
         }
     }
 
