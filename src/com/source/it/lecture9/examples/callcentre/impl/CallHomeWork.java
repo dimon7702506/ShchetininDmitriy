aef9783cf3910470086afc0080158730d7108692
dimon <C:\Users\dimon\AppData\Roaming\The Bat!>
2017-06-20 17:26:51 +0300

    '20170620'

--- src/com/source/it/lecture9/examples/callcentre/impl/CallHomeWork.java
+++ src/com/source/it/lecture9/examples/callcentre/impl/CallHomeWork.java
@@ -1,24 +1,23 @@
-package com.source.it.lecture9.examples.callcentre;
-
-
-import com.source.it.lecture9.examples.callcentre.impl.Call;
-import com.source.it.lecture9.examples.callcentre.impl.CallCentre;
+package com.source.it.lecture9.examples.callcentre.impl;
 
 import java.util.*;
 
-public class CallCentreTest2 {
+public class CallHomeWork {
     private static final int TOTAL_CALLS = 10;
     private static final String OPERATOR = "Operator";
 
     public static void main(String[] args) {
         List<Call> calls = new ArrayList<>(/*TOTAL_CALLS*/);// ToDo : choose collection impl
+        List<CallsDto> calls_Dto = new ArrayList<>();
 
         for (int i = 0; i < TOTAL_CALLS; i++) {
             calls.add(CallCentre.produceCall());
+
         }
         System.out.println(Arrays.toString(calls.toArray()));
         List<Integer> durations = Arrays.asList(0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
         Set<String> operators = new HashSet<>();
+
 
         for (Call call : calls) {
             int current = call.getDuration();
@@ -27,7 +26,17 @@ public class CallCentreTest2 {
             String operatorNumber = operator.substring(OPERATOR.length());
             int durationIndex = Integer.parseInt(operatorNumber) - 1;
             durations.set(durationIndex, current + durations.get(durationIndex));
+            calls_Dto.add(new CallsDto(operator, durations.get(durationIndex), 1));
         }
+
+        System.out.println("---------------------");
+        System.out.println(calls_Dto);
+        System.out.println("---------------------");
+
+        for (CallsDto callDto : calls_Dto) {
+
+        }
+
         for (String operator : operators) {
             String operatorNumber = operator.substring(OPERATOR.length());
             int durationIndex = Integer.parseInt(operatorNumber) - 1;
